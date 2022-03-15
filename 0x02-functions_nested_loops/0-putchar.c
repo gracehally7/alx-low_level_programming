fe79eabedf21044a5c8af0fe6df80563f3a244df
gracehally7 <gracehally7@gmail.com>
2022-03-15 05:57:41 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	-putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is for C functions. Particularly the nested loops
