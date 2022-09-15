52cc9d8083b74e400ef55e2866afbf48796d3d95
gracehally7 <99291785+gracehally7@users.noreply.github.com>
2022-09-15 01:14:14 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@ void print_alphabet_x10(void)
 	int a;
 	int c;
 
-	for (c=1; c <=10; c++)
+	for (c = 1; c <= 10; c++)
 	{
-		for (a='a'; a <= 'z'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		 {
 		    _putchar(a);
 		 }
-
 	         _putchar('\n');
 	 }
 }
