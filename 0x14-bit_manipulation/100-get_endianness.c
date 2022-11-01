51d54cbab771262ee5417fc05c791b30a0ef3609
gracehally7 <99291785+gracehally7@users.noreply.github.com>
2022-11-01 11:20:11 +0100

    binary

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int y;
+	char *c;
+
+	y = 1;
+	c = (char *) &y;
+
+	return ((int)*c);
+}
